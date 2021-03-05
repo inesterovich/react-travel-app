652b169b79e1f8f99c05967f0ef47a4a9cc018bd
Ilya Nesterovich <manager@inesterovich.ru>
2021-03-05 10:37:54 +0300

    feat: add server && launch scripts

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// Main Server file
+const express = require('express');
+const config = require('config');
+
+
+const server = express();
+
+
+const PORT = config.get('port') || 5000;
+
+
+async function start() {
+
+  try {
+    /*
+      await mongoose.connect(config.get('mongoUri'), {
+
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+      }) */
+
+      server.listen(PORT, () => console.log(`Server has been started on ${PORT}...`));
+      
+  } catch (error) {
+      console.log(error);
+      process.exit(1);
+  }
+
+
+  
+}
+
+start();
+
+
