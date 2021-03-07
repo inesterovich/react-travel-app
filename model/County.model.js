754ed0bb266686eaba7e0c2b32dad110731ab009
Ilya Nesterovich <manager@inesterovich.ru>
2021-03-07 21:33:47 +0300

    feat: get currency data, create sample function apiRequest

--- model/County.model.js
+++ model/County.model.js
@@ -43,7 +43,10 @@ const CountrySchema = new Schema({
   ],
 
   currency: {
-    
+    code: {
+     type: String
+   }
+
   },
 
   weather: {
--- routes/requests.routes.js
+++ routes/requests.routes.js
@@ -10,6 +10,7 @@ const { model } = require('mongoose');
 const countriesSampleResponse = require('./county_main_response.json');
 const citiesSampleResponse = require('./cities_example_response.json');
 const CitySchema = require('../model/City.model');
+const { base } = require('../model/County.model');
 
 
 const router = Router();
@@ -31,13 +32,74 @@ function stringfyCountries(array) {
 
 */
 
+
+// Нужен ли тут Content-Type?
 const authHeaders = {
-  'Content-Type': 'application/json',
   'X-Triposo-Account': 'R6UW3KQX',
   'X-Triposo-Token': 'pebxdd69doiwysybkqi5n3wuo9jsye7x'
 };
 
+
+
 const fieldsString = '&order_by=name&fields=name,id,intro,properties,images,snippet,coordinates&exclude_fields=id';
+
+/* Мне нужна функция apiRequest*/
+
+function apiRequest(
+  baseUri,
+  endPoint,
+  authData,
+  searchBy,
+  count = 10,
+  fields = 'all',
+  exclude,
+  trigram,
+  distance,
+  sortBy
+
+
+) {
+
+
+  if (typeof authData === 'object') {
+    /* есть searchBy [field, value],
+      есть include fields  string или простой массив значенич
+      есть exclude - это строка или массив значений.
+      есть annotate
+    */
+    if (searchBy && Array.isArray(searchBy)) {
+    //  searchBy = ['location_id', 'Russia'];
+      
+      searchBy = searchBy.join('=');
+    } else {
+      throw new Error('searchBy should be an array')
+    }
+
+    // fields = [id, images, intro, snippet, tags, location_id]
+
+    if ( fields && Array.isArray(fields)) {
+
+      fields = `fields=${fields.map(field => field.replace(' ', ' ')).join()}`
+    } else if (typeof fields === 'string') {
+      fields = `fields=${fields}`;
+    } else {
+      throw new Error('fields should be array or string with "all" value')
+    }
+
+    if (typeof count !== 'number') {
+      throw new Error('count expected to be a number');
+    }
+    
+    const queryString =
+      `${baseUri}/${endPoint}.json?${searchBy}&count=${count}&${fields}`;
+    
+    return queryString;
+  }
+
+  // В конечном итоге, от функции я ожидаю, что она будет возвращать готовый объект для функции fetch
+  
+
+}
 
 router.post('/getCountries', async (req, res) => {
 
@@ -75,10 +137,10 @@ router.post('/getCountries', async (req, res) => {
         },
         properties: countryObject.properties,
         coordinates: countryObject.coordinates,
-        currency: {},
-        weather: {
-
+        currency: {
+          code: '--'
         },
+        
        
       }
     })
@@ -175,6 +237,52 @@ router.post('/getCities', async (req, res) => {
   } catch (error) {
     console.log(error.message)
   }
+})
+
+router.post('/getCurrencies', async (req, res) => {
+  try {
+    const countries = await CountryModel.find({});
+    const currencies = countries.map(country => {
+      return {
+        _id: country._id,
+        code: country.currency.code,
+        
+      }
+    });
+
+    const requests = currencies.map((currencyObj) => {
+      const apiKey = '06d6831204e1450b40012dc2';
+      const baseUri = 'https://v6.exchangerate-api.com'
+      const fetchUri = `${baseUri}/v6/${apiKey}/latest/${currencyObj.code}`;
+
+      return fetch(fetchUri);
+    });
+
+    const apiResponse = await Promise.all(requests);
+    const apiData = await Promise.all(apiResponse.map(response => response.json()));
+    
+    if (resu) {}
+
+    
+
+    res.json(apiData);
+
+    
+  } catch (error) {
+    
+  }
+})
+
+router.post('/getAttractions', async (req, res) => {
+  const baseUri = 'https://www.triposo.com/api/20201111';
+  const endPoint = 'poi';
+  const searchBy = ['location_id', 'Moscow'];
+  const count = 6;
+
+  const fields = ['id', 'images', 'name', 'intro', 'snippet', 'tags', 'location_id'];
+  const query = apiRequest(baseUri, endPoint, authHeaders, searchBy, count, fields);
+
+  res.json(query);
 })
 
 module.exports = router;
