fe694d17112f81da406ccf68bcee949a37c84e4f
REAZZY <maxim.nebela@gmail.com>
2021-03-09 23:42:05 +0200

    fix: change a tag to Link

--- client/src/views/components/CountryCard/CountryCard.tsx
+++ client/src/views/components/CountryCard/CountryCard.tsx
@@ -7,35 +7,39 @@ import Typography from "@material-ui/core/Typography";
 import ApartmentIcon from "@material-ui/icons/Apartment";
 import Rating from "@material-ui/lab/Rating";
 import Russia from "./Russia.jpg";
+import React from "react";
+import {Link} from "react-router-dom";
 
 const CountryCard: React.FC = () => (
   <Card>
-    <CardActionArea href="/country">
-      <CardMedia
-        component="img"
-        alt="Contemplative Reptile"
-        height="140"
-        image={Russia}
-        title="Contemplative Reptile"
-      />
-      <CardContent className={styles.CardContent}>
-        <div className={styles.meta}>
-          <Typography className={styles.meta__country} component="h2">
-            Россия
-          </Typography>
-          <div className={styles.meta__city}>
-            Москва <ApartmentIcon></ApartmentIcon>
+    <Link to={"/country"} className={styles.link}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={Russia}
+          title="Contemplative Reptile"
+        />
+        <CardContent className={styles.CardContent}>
+          <div className={styles.meta}>
+            <Typography className={styles.meta__country} component="h2">
+              Россия
+            </Typography>
+            <div className={styles.meta__city}>
+              Москва <ApartmentIcon></ApartmentIcon>
+            </div>
           </div>
-        </div>
-        <Typography variant="body2" color="textSecondary" component="p">
-          Россия – крупнейшая страна мира, расположенная в Восточной Европе и
-          Северной Азии и омываемая водами Тихого и Северного Ледовитого
-          океанов. Ландшафт России крайне разнообразен: на ее территории есть и
-          тундра, и леса, и субтропические пляжи...
-        </Typography>
-        <Rating name="read-only" value={3} readOnly size="small" />
-      </CardContent>
-    </CardActionArea>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Россия – крупнейшая страна мира, расположенная в Восточной Европе и
+            Северной Азии и омываемая водами Тихого и Северного Ледовитого
+            океанов. Ландшафт России крайне разнообразен: на ее территории есть и
+            тундра, и леса, и субтропические пляжи...
+          </Typography>
+          <Rating name="read-only" value={3} readOnly size="small" />
+        </CardContent>
+      </CardActionArea>
+    </Link>
   </Card>
 );
 
