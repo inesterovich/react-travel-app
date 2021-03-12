a420a67dcda8d33654192e597d28db6b21933dfe
REAZZY <maxim.nebela@gmail.com>
2021-03-12 11:50:40 +0200

    feat: add capital search

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import styles from "./styles.module.css";
 
-import { CircularProgress, Fade } from "@material-ui/core";
+import {CircularProgress, Fade} from "@material-ui/core";
 import CountryCard from "../../views/components/CountryCard/index";
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 
-import { ICountry } from "../../types";
+import {ICountry} from "../../types";
 
-import { getCountriesThunk } from "../../redux/countries";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../redux/rootReducer";
+import {getCountriesThunk} from "../../redux/countries";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../redux/rootReducer";
 
 interface IProps {
   countries: {
@@ -23,28 +23,32 @@ const MainPage: React.FC = () => {
   const countries = useSelector((state: IProps) => state.countries || []);
   const dispatch = useDispatch();
   const search = useSelector((state: RootState) => state.countries.search);
+  const currentLanguage = useSelector((state: RootState) => state.countries.currentLanguage)
   useEffect(() => {
     if (!countries.data.length) dispatch(getCountriesThunk());
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  console.log(countries.data)
   return (
     <div className={styles.mainPage}>
       {countries.isLoading ? (
         <div className="center_block">
-          <CircularProgress />
+          <CircularProgress/>
         </div>
       ) : (
         <Fade in={true} timeout={700}>
           <div className="flex-wrap">
             {countries.data
               .filter((e) =>
-                e.name.toLowerCase().includes(search.trim().toLowerCase())
+                e[`${currentLanguage}`].name?.toLowerCase().includes(search.trim().toLowerCase())
+                || e[`${currentLanguage}`].capital?.name?.toLowerCase().includes(search.trim().toLowerCase())
               )
               .map((el: ICountry) => (
                 <div className="flex-wrap__item" key={uuidv4()}>
                   <CountryCard
-                    name={el.name}
-                    description={el.description}
-                    image={el.image}
+                    name={el[`${currentLanguage}`].name}
+                    description={el[`${currentLanguage}`].description}
+                    image={el[`${currentLanguage}`].image}
+                    capital={el[`${currentLanguage}`].capital}
                   />
                 </div>
               ))}
--- client/src/views/components/Search/Search.tsx
+++ client/src/views/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import styles from "./styles.module.css";
 import SearchBar from "material-ui-search-bar";
 import {useDispatch, useSelector} from "react-redux";
@@ -8,11 +8,27 @@ import {RootState} from "../../../redux/rootReducer";
 const Search: React.FC = React.memo(() => {
   const dispatch = useDispatch()
   const search = useSelector((state:RootState) => state.countries.search)
+  const searchRef = useRef(null)
+  useEffect(()=>{
+    //@ts-ignore
+    searchRef.current && searchRef.current.focus()
+  }, [searchRef])
   const handleChange = (e: string) => {
     dispatch(actionSetSearch(e))
   }
+  const handleClean = () => {
+    dispatch(actionSetSearch(""))
+  }
+
   return (
-    <SearchBar onChange={handleChange} value={search} className={styles.search}/>
+    <SearchBar
+      onChange={handleChange}
+      value={search}
+      className={styles.search}
+      onCancelSearch={handleClean}
+      //@ts-ignore
+      ref={searchRef}
+    />
   )
 });
 export default Search;
