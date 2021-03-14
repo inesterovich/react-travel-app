f8559457597bb4e28c9bc38b856fe02a99ed4596
Yakimovich Kanstantsin <yakimovichkostya@gmail.com>
2021-03-15 00:57:02 +0300

    feat: add modal reiting

--- client/src/pages/content/components/PhotoGallery/PhotoGallery.tsx
+++ client/src/pages/content/components/PhotoGallery/PhotoGallery.tsx
@@ -5,6 +5,8 @@ import { useSelector } from "react-redux";
 import { CurrentCountry, CurrentCountryLang } from "../../../../types";
 import { RootState } from "../../../../redux/rootReducer";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Rating from "@material-ui/lab/Rating";
+import Modal from "@material-ui/core/Dialog";
 import fullscreen from "../../../../assets/img/fullscreen.png";
 import SwiperCore, {
   Navigation,
@@ -15,6 +17,8 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/swiper-bundle.css";
 import styles from "./styles.module.css";
+import { Button } from "@material-ui/core";
+import { useHttp } from "../../../../hooks/http.hook";
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectCube]);
 enum Lang {
   Ru = "ru",
@@ -30,6 +34,8 @@ type State = {
 };
 
 const PhotoGallery: React.FC = React.memo(() => {
+  const { request } = useHttp();
+
   const currentCountry = useSelector((state: State) => {
     return state.countries.currentCountry || [];
   });
@@ -69,6 +75,37 @@ const PhotoGallery: React.FC = React.memo(() => {
       setImages(imagesSlider);
     }
   }, [countryLangData]);
+  const [valueReiting, setValueReiting] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [attractionID, setAttractionID] = useState("");
+  const handleClickOpen = useCallback((id) => {
+    setOpen(true);
+    setAttractionID(id);
+    setValueReiting(0);
+  }, []);
+
+  const handleClose = useCallback((value) => {
+    setOpen(false);
+  }, []);
+
+  const handleChangeReiting = useCallback((e) => {
+    setValueReiting(e.target.value);
+  }, []);
+
+  const handleClickSave = useCallback(async () => {
+    const resultReiting = {
+      countryId: currentCountry.en._id,
+      attractionId: attractionID,
+      lang: currentLanguage,
+      value: valueReiting,
+    };
+
+    try {
+      const data = await request("/api/service/vote", "POST", resultReiting);
+      console.log(data);
+    } catch (error) {}
+    setOpen(false);
+  }, [valueReiting, currentCountry, attractionID, currentLanguage, request]);
 
   return (
     <div className={styles.photoGallery}>
@@ -84,14 +121,55 @@ const PhotoGallery: React.FC = React.memo(() => {
         >
           {countryLangData?.attractions?.map((item: any) => {
             return (
-              <SwiperSlide key={item._id}>
-                <img src={item.url} alt="slider" className={styles.img} />
-                <p className={styles.textImg}>{item.name}</p>
-              </SwiperSlide>
+              <>
+                <SwiperSlide key={item._id}>
+                  <img src={item.url} alt="slider" className={styles.img} />{" "}
+                  <p className={styles.textImg}>{item.name}</p>{" "}
+                  <div className={styles.vote}>
+                    {" "}
+                    <Rating
+                      name="read-only"
+                      value={3}
+                      readOnly
+                      size="small"
+                      className={styles.meta__rating}
+                    />
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handleClickOpen(item._id)}
+                    >
+                      Vote
+                    </Button>
+                  </div>
+                </SwiperSlide>{" "}
+              </>
             );
           })}
         </Swiper>
-
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          className={styles.modal}
+        >
+          <Rating
+            name="hover-feedback"
+            precision={1}
+            size="large"
+            onChange={handleChangeReiting}
+            className={styles.modalReiting}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={styles.btnRating}
+            onClick={handleClickSave}
+          >
+            SAVE
+          </Button>
+        </Modal>
         {images.length && (
           <ReactBnbGallery
             photos={images}
